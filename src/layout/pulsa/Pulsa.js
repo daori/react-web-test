fdffdbfc08499dc08f57caba440f4da1d259fdc0
David Senjaya <davidsenjaya@netzme.id>
2017-08-30 13:50:48 +0700

    Coba react tab

--- src/layout/pulsa/Pulsa.js
+++ src/layout/pulsa/Pulsa.js
@@ -1,22 +1,35 @@
 import React, { Component } from 'react';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
 
 
 export default class Pulsa extends React.Component {
 
     constructor(){
         super();
-        this.state = {tabIndex: 0};
+        this.state = 
+        {
+            tabIndex: 0,
+            tabs: ["Tabs1", "Tabs2", "Tabs3"]
+        };
+    
     }
+
     render() {
+    
         return (
             <Tabs selectedIndex={this.state.tabIndex} onSelect={tabIndex => this.setState({ tabIndex })}>
                 <TabList>
-                    <Tab>Title 1</Tab>
-                    <Tab>Title 2</Tab>
+                {this.state.tabs.map(function(tab) {
+                    return <Tab>{tab}</Tab>
+                })}
                 </TabList>
-                <TabPanel></TabPanel>
-                <TabPanel></TabPanel>
+                <TabPanel>
+                    <h2>Any content 1</h2>
+                </TabPanel>
+                <TabPanel>
+                    <h2>Any content 2</h2>
+                </TabPanel>
             </Tabs>
 
         );
